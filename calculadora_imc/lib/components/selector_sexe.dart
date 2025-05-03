bcacf3abbfe1fb2b8d0dc6b2451f8e434fbdb2e3
Yago Yàsser <yagoyasser2001@gmail.com>
2025-05-03 20:58:16 +0200

    Implementació accions botons sexe

--- calculadora_imc/lib/components/selector_sexe.dart
+++ calculadora_imc/lib/components/selector_sexe.dart
@@ -19,68 +19,90 @@ class _SelectorSexeState extends State<SelectorSexe> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Expanded(
-          child: Padding(
-            padding: EdgeInsets.only(left: Paddings.paddingGran, top: Paddings.paddingGran, right: Paddings.paddingPetit, bottom: Paddings.paddingGran),
-            child: Container(
-              decoration: BoxDecoration(
-                color: sexeSeleccionat == "Mascle"
-                  ? AppColors.fonsComponentSeleccionat
-                  : AppColors.fonsComponent,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Padding(
-                // L'ideal seria obtindre la mida més gran entre l'alt i l'ample, sumar-li els padding i aplicar eixa nova mida al valor més menut d'entre l'alt i l'ample
-                // En aquest cas, com es pot establir les mides de les imatges, paddings i texts, no és estrictament necessari fer-ho així
-                // A més, com en aquest cas s'implementa Expanded, dona igual ja que Expanded té una mida major que la del Padding
-                padding: EdgeInsets.only(left: Paddings.paddingGran, top: Paddings.paddingPetit, right: Paddings.paddingGran),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "imatges/mascle.png",
-                      height: 100,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: Paddings.paddingPetit),
-                      child: Text(
-                        "Mascle",
-                        style: EstilsTexts.cosText,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                if (sexeSeleccionat == "Mascle") {
+                  sexeSeleccionat = null;
+                } else {
+                  sexeSeleccionat = "Mascle";
+                }
+              });
+            },
+            child: Padding(
+              padding: EdgeInsets.only(left: Paddings.paddingGran, top: Paddings.paddingGran, right: Paddings.paddingPetit, bottom: Paddings.paddingGran),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: sexeSeleccionat == "Mascle"
+                    ? AppColors.fonsComponentSeleccionat
+                    : AppColors.fonsComponent,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  // L'ideal seria obtindre la mida més gran entre l'alt i l'ample, sumar-li els padding i aplicar eixa nova mida al valor més menut d'entre l'alt i l'ample
+                  // En aquest cas, com es pot establir les mides de les imatges, paddings i texts, no és estrictament necessari fer-ho així
+                  // A més, com en aquest cas s'implementa Expanded, dona igual ja que Expanded té una mida major que la del Padding
+                  padding: EdgeInsets.only(left: Paddings.paddingGran, top: Paddings.paddingPetit, right: Paddings.paddingGran),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        "imatges/mascle.png",
+                        height: 100,
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: Paddings.paddingPetit),
+                        child: Text(
+                          "Mascle",
+                          style: EstilsTexts.cosText,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
         ),
         Expanded(
-          child: Padding(
-            padding: EdgeInsets.only(left: Paddings.paddingPetit, top: Paddings.paddingGran, right: Paddings.paddingGran, bottom: Paddings.paddingGran),
-            child: Container(
-              decoration: BoxDecoration(
-                color: sexeSeleccionat == "Femella"
-                  ? AppColors.fonsComponentSeleccionat
-                  : AppColors.fonsComponent,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Padding(
-                // L'ideal seria obtindre la mida més gran entre l'alt i l'ample, sumar-li els padding i aplicar eixa nova mida al valor més menut d'entre l'alt i l'ample
-                // En aquest cas, com es pot establir les mides de les imatges, paddings i texts, no és estrictament necessari fer-ho així
-                // A més, com en aquest cas s'implementa Expanded, dona igual ja que Expanded té una mida major que la del Padding
-                padding: EdgeInsets.only(left: Paddings.paddingGran, top: Paddings.paddingPetit, right: Paddings.paddingGran),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "imatges/femella.png",
-                      height: 100,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: Paddings.paddingPetit),
-                      child: Text(
-                        "Femella",
-                        style: EstilsTexts.cosText,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                if (sexeSeleccionat == "Femella") {
+                  sexeSeleccionat = null;
+                } else {
+                  sexeSeleccionat = "Femella";
+                }
+              });
+            },
+            child: Padding(
+              padding: EdgeInsets.only(left: Paddings.paddingPetit, top: Paddings.paddingGran, right: Paddings.paddingGran, bottom: Paddings.paddingGran),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: sexeSeleccionat == "Femella"
+                    ? AppColors.fonsComponentSeleccionat
+                    : AppColors.fonsComponent,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  // L'ideal seria obtindre la mida més gran entre l'alt i l'ample, sumar-li els padding i aplicar eixa nova mida al valor més menut d'entre l'alt i l'ample
+                  // En aquest cas, com es pot establir les mides de les imatges, paddings i texts, no és estrictament necessari fer-ho així
+                  // A més, com en aquest cas s'implementa Expanded, dona igual ja que Expanded té una mida major que la del Padding
+                  padding: EdgeInsets.only(left: Paddings.paddingGran, top: Paddings.paddingPetit, right: Paddings.paddingGran),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        "imatges/femella.png",
+                        height: 100,
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: Paddings.paddingPetit),
+                        child: Text(
+                          "Femella",
+                          style: EstilsTexts.cosText,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
